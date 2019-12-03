217478491d4a81ab773a849eda0ce57fb2d0e177
RonKbS <stephen.bamwite@gmail.com>
2019-12-03 17:29:14 +0300

    - create chart for pivot 1 and its subplots

--- js/farms.js
+++ js/farms.js
@@ -1,13 +1,10 @@
 
-const pivots = [
-  'Pivot 1', 'Pivot 2',
-  'Pivot 3', 'Pivot 4',
-  'Pivot 5', 'Pivot 6',
-  'Pivot 7', 'Pivot 8',
-  'Pivot 9', 'Pivot 10',
-]
+
+let set_mgt_zones = new Set()
+let mgt_zones = []
+
 let ndvi_mean_date = [];
-let ndvi_mean_values = [];
+let ndvi_mean_data = [];
 
 let jsonUrl_ = "https://geogecko.gis-cdn.net/geoserver/Olam_Vector/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Olam_Vector:Isanya&outputFormat=text/javascript&format_options=callback:handleWebsiteJson_";
 $.ajax(jsonUrl_,
@@ -15,17 +12,44 @@ $.ajax(jsonUrl_,
 ).done(() => { });
 
 function handleWebsiteJson_(data) {
+  // get date values...hopefully they are the same..tihi
   let ndvi_mean_std = Object.keys(data.features[0].properties).slice(start=4)
-  let ndvi_mean_std_values = Object.values(data.features[0].properties).slice(start=4)
-
   ndvi_mean_std.forEach(e => {
     if (e.slice(start=e.length - 4) === 'mean'){
       let date_ = e.split("_")[1];
       date_ = date_.slice(0, 4) + '-' + date_.slice(4, 6) + '-' + date_.slice(start=6)
       ndvi_mean_date.push(date_);
-      ndvi_mean_values.push(data.features[0].properties[e]);
-    }
+    };
   });
+
+  data.features.forEach(feature => {
+
+    // get mgt zones as unique strings for the buttons
+    set_mgt_zones.add(feature.properties.Name)
+    mgt_zones = Array.from(set_mgt_zones)
+
+
+    if (feature.properties.Name == 'Pivot 1') {
+      let ndvi_mean_std_ = Object.keys(feature.properties).slice(start=4)
+    
+      let these_values = []
+      ndvi_mean_std_.forEach(e => {
+        if (e.slice(start=e.length - 4) === 'mean'){
+          let date_ = e.split("_")[1];
+          these_values.push(feature.properties[e]);
+        };
+      });
+      ndvi_mean_data.push({
+        type: 'scatter',
+        mode: 'lines+marker',
+        line: {shape: 'spline'},
+        name: `unit_id_${feature.properties.unit_id}`,
+        x: ndvi_mean_date,
+        y: these_values
+      });
+    };  
+  });
+
 }
 
 
@@ -33,18 +57,7 @@ function handleWebsiteJson_(data) {
 
 window.onload = function() {
 
-
-
-  console.log('here we are...')
-  var data = [
-    {
-      x: ndvi_mean_date,
-      y: ndvi_mean_values,
-      type: 'scatter',
-      mode: 'lines+marker',
-      line: {shape: 'spline'},
-    }
-  ];
   
-  Plotly.newPlot('chartContainer', data);
+  Plotly.newPlot('chartContainer', ndvi_mean_data);
+
 }
