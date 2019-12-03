92928f9266f3f1503a050f3c5a62f8c634012b53
RonKbS <stephen.bamwite@gmail.com>
2019-12-03 15:22:31 +0300

    - plot mean ndvi graph for pivot 1 data subplot

--- js/farms.js
+++ js/farms.js
@@ -1,322 +1,50 @@
 
-  window.onload = function() {
+const pivots = [
+  'Pivot 1', 'Pivot 2',
+  'Pivot 3', 'Pivot 4',
+  'Pivot 5', 'Pivot 6',
+  'Pivot 7', 'Pivot 8',
+  'Pivot 9', 'Pivot 10',
+]
+let ndvi_mean_date = [];
+let ndvi_mean_values = [];
 
-    var chart = new CanvasJS.Chart("chartContainer", {
-      backgroundColor: "#fff",
-      fontFamily: "Rubix",
-      animationEnabled: true,
-      theme: "light2",
-      title: {},
-      axisX: {
-        valueFormatString: "DD MMM",
-        crosshair: {
-          enabled: true,
-          snapToDataPoint: true
-        }
-      },
-      axisY: {
-        title: "NDVI",
-        crosshair: {
-          enabled: true
-        }
-      },
-      toolTip: {
-        shared: true
-      },
-      legend: {
-        cursor: "pointer",
-        verticalAlign: "bottom",
-        horizontalAlign: "left",
-        dockInsidePlotArea: true,
-        itemclick: toogleDataSeries
-      },
-      data: [{
-          type: "spline",
-          lineThickness: 1,
-          showInLegend: true,
-          name: "Isanya",
-          markerType: "circle",
-          xValueFormatString: "DD MMM, YYYY",
-          color: "red",
-          dataPoints: [{
-              x: new Date(2019, 7, 4),
-              y: 0.6
-            },
-            {
-              x: new Date(2019, 7, 9),
-              y: 0.7
-            },
-            {
-              x: new Date(2019, 7, 14),
-              y: 0.1
-            },
-            {
-              x: new Date(2019, 7, 19),
-              y: 0.8
-            },
-            {
-              x: new Date(2019, 7, 24),
-              y: -1
-            },
-            {
-              x: new Date(2019, 7, 29),
-              y: 0.9
-            },
-            {
-              x: new Date(2019, 8, 3),
-              y: 0.4
-            },
-            {
-              x: new Date(2019, 8, 13),
-              y: 0.53
-            },
-            {
-              x: new Date(2019, 8, 18),
-              y: 0.69
-            }
-          ]
-        },
-        {
-          type: "spline",
-          lineThickness: 1,
-          showInLegend: true,
-          name: "Kateshi",
-          markerType: "circle",
-          xValueFormatString: "DD MMM, YYYY",
-          color: "#808080",
-          dataPoints: [{
-              x: new Date(2019, 7, 4),
-              y: 0.50
-            },
-            {
-              x: new Date(2019, 7, 9),
-              y: 0.7
-            },
-            {
-              x: new Date(2019, 7, 14),
-              y: 0.71
-            },
-            {
-              x: new Date(2019, 7, 19),
-              y: 0.9
-            },
-            {
-              x: new Date(2019, 7, 24),
-              y: 0.34
-            },
-            {
-              x: new Date(2019, 7, 29),
-              y: 1
-            },
-            {
-              x: new Date(2019, 8, 3),
-              y: 0.8
-            },
-            {
-              x: new Date(2019, 8, 13),
-              y: 0.57
-            },
-            {
-              x: new Date(2019, 8, 18),
-              y: 0.79
-            }
-          ]
-        },
-        {
-          type: "spline",
-          lineThickness: 1,
-          showInLegend: true,
-          name: "Lukulu",
-          markerType: "circle",
-          xValueFormatString: "DD MMM, YYYY",
-          color: "#808080",
-          dataPoints: [{
-              x: new Date(2019, 7, 4),
-              y: 0.4
-            },
-            {
-              x: new Date(2019, 7, 9),
-              y: 0.2
-            },
-            {
-              x: new Date(2019, 7, 14),
-              y: 0.4
-            },
-            {
-              x: new Date(2019, 7, 19),
-              y: 0.8
-            },
-            {
-              x: new Date(2019, 7, 24),
-              y: 0.2
-            },
-            {
-              x: new Date(2019, 7, 29),
-              y: 0.5
-            },
-            {
-              x: new Date(2019, 8, 3),
-              y: 0.7
-            },
-            {
-              x: new Date(2019, 8, 13),
-              y: 0.1
-            },
-            {
-              x: new Date(2019, 8, 18),
-              y: 0.3
-            }
-          ]
-        },
-        {
-          type: "spline",
-          lineThickness: 1,
-          showInLegend: true,
-          name: "Luombe",
-          markerType: "circle",
-          xValueFormatString: "DD MMM, YYYY",
-          color: "#808080",
-          dataPoints: [{
-              x: new Date(2019, 7, 4),
-              y: 0.9
-            },
-            {
-              x: new Date(2019, 7, 9),
-              y: 0.4
-            },
-            {
-              x: new Date(2019, 7, 14),
-              y: 0.71
-            },
-            {
-              x: new Date(2019, 7, 19),
-              y: 0.2
-            },
-            {
-              x: new Date(2019, 7, 24),
-              y: 0.4
-            },
-            {
-              x: new Date(2019, 7, 29),
-              y: 0.9
-            },
-            {
-              x: new Date(2019, 8, 3),
-              y: 0.8
-            },
-            {
-              x: new Date(2019, 8, 13),
-              y: 0.5
-            },
-            {
-              x: new Date(2019, 8, 18),
-              y: 0.2
-            }
-          ]
-        },
-        {
-          type: "spline",
-          lineThickness: 1,
-          showInLegend: true,
-          name: "Ngoli",
-          markerType: "circle",
-          xValueFormatString: "DD MMM, YYYY",
-          color: "#808080",
-          dataPoints: [{
-              x: new Date(2019, 7, 4),
-              y: 0.7
-            },
-            {
-              x: new Date(2019, 7, 9),
-              y: 0.4
-            },
-            {
-              x: new Date(2019, 7, 14),
-              y: 1.0
-            },
-            {
-              x: new Date(2019, 7, 19),
-              y: -1.3
-            },
-            {
-              x: new Date(2019, 7, 24),
-              y: 0.9
-            },
-            {
-              x: new Date(2019, 7, 29),
-              y: 0.06
-            },
-            {
-              x: new Date(2019, 8, 3),
-              y: 0.4
-            },
-            {
-              x: new Date(2019, 8, 13),
-              y: 0.6
-            },
-            {
-              x: new Date(2019, 8, 18),
-              y: 0.4
-            }
-          ]
-        },
-        {
-          type: "spline",
-          lineThickness: 1,
-          showInLegend: true,
-          name: "Nsuzu",
-          markerType: "circle",
-          xValueFormatString: "DD MMM, YYYY",
-          color: "#808080",
-          dataPoints: [{
-              x: new Date(2019, 7, 4),
-              y: 0.5
-            },
-            {
-              x: new Date(2019, 7, 9),
-              y: 0.10
-            },
-            {
-              x: new Date(2019, 7, 14),
-              y: 0.2
-            },
-            {
-              x: new Date(2019, 7, 19),
-              y: 0.9
-            },
-            {
-              x: new Date(2019, 7, 24),
-              y: 0.01
-            },
-            {
-              x: new Date(2019, 7, 29),
-              y: 0.9
-            },
-            {
-              x: new Date(2019, 8, 3),
-              y: 0.6
-            },
-            {
-              x: new Date(2019, 8, 13),
-              y: 0.3
-            },
-            {
-              x: new Date(2019, 8, 18),
-              y: 0.06
-            }
-          ]
-        }
-      ]
-    });
-    chart.render();
+let jsonUrl_ = "https://geogecko.gis-cdn.net/geoserver/Olam_Vector/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Olam_Vector:Isanya&outputFormat=text/javascript&format_options=callback:handleWebsiteJson_";
+$.ajax(jsonUrl_,
+    { dataType: "jsonp" }
+).done(() => { });
 
-    function toogleDataSeries(e) {
-      if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-        e.dataSeries.visible = false;
-      } else {
-        e.dataSeries.visible = true;
-      }
-      chart.render();
+function handleWebsiteJson_(data) {
+  let ndvi_mean_std = Object.keys(data.features[0].properties).slice(start=4)
+  let ndvi_mean_std_values = Object.values(data.features[0].properties).slice(start=4)
+
+  ndvi_mean_std.forEach(e => {
+    if (e.slice(start=e.length - 4) === 'mean'){
+      let date_ = e.split("_")[1];
+      date_ = date_.slice(0, 4) + '-' + date_.slice(4, 6) + '-' + date_.slice(start=6)
+      ndvi_mean_date.push(date_);
+      ndvi_mean_values.push(data.features[0].properties[e]);
     }
+  });
+}
 
-  }
+
+
+
+window.onload = function() {
+
+
+
+  console.log('here we are...')
+  var data = [
+    {
+      x: ndvi_mean_date,
+      y: ndvi_mean_values,
+      type: 'scatter',
+      mode: 'lines+marker',
+      line: {shape: 'spline'},
+    }
+  ];
+  
+  Plotly.newPlot('chartContainer', data);
+}
