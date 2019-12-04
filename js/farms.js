514db3f2c7510777cd8b81b144223f12e0348e8a
RonKbS <stephen.bamwite@gmail.com>
2019-12-04 12:49:04 +0300

    - allow choosing of different pivots on line chart

--- js/farms.js
+++ js/farms.js
@@ -5,13 +5,24 @@ let mgt_zones = []
 
 let ndvi_mean_date = [];
 let ndvi_mean_data = [];
+let setZonePlot;
+
+let mgtZoneSelector = document.querySelector('.zonedata');
+function assignOptions(textArray, selector) {
+  textArray.forEach(i => {
+      let currentOption = document.createElement('option');
+      currentOption.text = i;
+      selector.appendChild(currentOption);
+  })
+}
+
 
 let jsonUrl_ = "https://geogecko.gis-cdn.net/geoserver/Olam_Vector/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Olam_Vector:Isanya&outputFormat=text/javascript&format_options=callback:handleWebsiteJson_";
 $.ajax(jsonUrl_,
     { dataType: "jsonp" }
 ).done(() => { });
 
-function handleWebsiteJson_(data) {
+ function handleWebsiteJson_(data) {
   // get date values...hopefully they are the same..tihi
   let ndvi_mean_std = Object.keys(data.features[0].properties).slice(start=4)
   ndvi_mean_std.forEach(e => {
@@ -22,42 +33,77 @@ function handleWebsiteJson_(data) {
     };
   });
 
+  // get mgt zones as unique strings for the buttons
   data.features.forEach(feature => {
-
-    // get mgt zones as unique strings for the buttons
     set_mgt_zones.add(feature.properties.Name)
     mgt_zones = Array.from(set_mgt_zones)
-
-
-    if (feature.properties.Name == 'Pivot 1') {
-      let ndvi_mean_std_ = Object.keys(feature.properties).slice(start=4)
-    
-      let these_values = []
-      ndvi_mean_std_.forEach(e => {
-        if (e.slice(start=e.length - 4) === 'mean'){
-          let date_ = e.split("_")[1];
-          these_values.push(feature.properties[e]);
-        };
-      });
-      ndvi_mean_data.push({
-        type: 'scatter',
-        mode: 'lines+marker',
-        line: {shape: 'spline'},
-        name: `unit_id_${feature.properties.unit_id}`,
-        x: ndvi_mean_date,
-        y: these_values
-      });
-    };  
   });
 
+  setZonePlot = (zone) => {
+    
+    data.features.forEach(feature => {
+
+      if (feature.properties.Name == zone) {
+        let ndvi_mean_std_ = Object.keys(feature.properties).slice(start=4)
+      
+        let these_values = []
+        ndvi_mean_std_.forEach(e => {
+          if (e.slice(start=e.length - 4) === 'mean'){
+            let date_ = e.split("_")[1];
+            these_values.push(feature.properties[e]);
+          };
+        });
+        ndvi_mean_data.push({
+          type: 'scatter',
+          mode: 'lines+marker',
+          line: {shape: 'spline'},
+          name: `unit_id_${feature.properties.unit_id}`,
+          x: ndvi_mean_date,
+          y: these_values
+        });
+      };  
+    });
+  
+  
+  Plotly.newPlot('chartContainer', ndvi_mean_data, {
+    title: {
+      text:'Farm Performance',
+      font: {
+        family: 'Times New, roman',
+        size: 24
+      }
+    },
+    autosize: false,
+    width: 1030,
+    height:700,},
+  { showSendToCloud: true }
+  );
+};
+
+
 }
-
-
 
 
 window.onload = function() {
 
-  
-  Plotly.newPlot('chartContainer', ndvi_mean_data);
+  mgt_zones.sort((a, b) => {
+    a = parseInt(a.split(' ')[1]);
+    b = parseInt(b.split(' ')[1]);
+    if (a < b) {
+      return -1;
+    } else if ( a > b) {
+      return 1;
+    }
+    return 0;
+  });
+  assignOptions(mgt_zones, mgtZoneSelector);
 
-}
+
+  // initial plot
+  setZonePlot(mgt_zones[0]);
+
+  function updateZone() {
+    setZonePlot(mgtZoneSelector.value);
+  };
+  mgtZoneSelector.addEventListener('change', updateZone, false);
+};
