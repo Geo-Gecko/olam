41fb254304f8ac2746a75ae977366b51c99c82df
RonKbS <stephen.bamwite@gmail.com>
2020-01-20 15:07:08 +0300

    pick csvs from gcp storage

--- utilities/preptempmoi.js
+++ utilities/preptempmoi.js
@@ -34,9 +34,9 @@ let chart_ = (data_, dates_, title_, chart_id) => Plotly.newPlot(
 
 
 let files_ = place => [
-    axios.get(`csvs/${place}_DayTemperature.csv`),
-    axios.get(`csvs/${place}_Precipitation.csv`),
-    axios.get(`csvs/${place}_SoilMoisture.csv`)
+    axios.get(`https://storage.googleapis.com/olam-cvs/${place}_DayTemperature.csv`),
+    axios.get(`https://storage.googleapis.com/olam-cvs/${place}_Precipitation.csv`),
+    axios.get(`https://storage.googleapis.com/olam-cvs/${place}_SoilMoisture.csv`)
 ]
 
 
